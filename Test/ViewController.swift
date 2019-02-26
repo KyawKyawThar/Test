6f473af1101f8e54bd5b7e319609287c7cd05152
Kyaw Kyaw <kyawkyaw@Kyaws-MacBook-Pro.local>
2019-02-26 18:08:36 +0630

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Kyaw Kyaw on 2/26/19.
+//  Copyright © 2019 Kyaw Kyaw Thar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
